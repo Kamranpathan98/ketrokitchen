f10c3d424ec050fed138d78fa95c19e2cf322a10
Kamranpathan98 <kamranpathan98@gmail.com>
2025-01-13 18:30:08 +0530

    removed the unwanted codes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack screenOptions={{headerShown: false}}>
         <Stack.Screen name="(home)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
---
+++ types/Datatypes.tsx
@@ -0,0 +1,7 @@
+export interface DataItem {
+    name: string;
+    uri?: string; // URI for images
+    description?: string; // Description for the item
+    price?: string; // Price for the item
+    rating?: number; // Rating for the item
+  }
